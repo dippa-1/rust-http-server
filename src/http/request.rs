86f8e15564d39b11fd3297154782bd55bd7f4c1b
dippa-1 <dominik.helfenstein@gmx.de>
2022-05-16 17:20:33 +0200

    add trait for request convert

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,12 @@
 use super::method::Method;
+use std::convert::TryFrom;
+
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
 }
